9ddf8d031b585d9f8f064d217527d77fa85e5d3b
dmlv <davismedins@gmail.com>
2024-01-06 22:30:02 +0200

    Added static folder and build

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@
  const products = require('./routes/api/products');
 
  app.use('/api/products',products);
+
+ if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+
+    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'));
+ }
+
  const port = process.env.PORT || 5001;
 
  app.listen(port, () => console.log(`Server running on ${port}`));
